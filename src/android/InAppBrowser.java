49338be787e4976c029e8634bd27bf13c72408cf
Erika gonzalez <ejgonzalez971@misena.edu.co>
2018-08-28 11:27:24 -0500

    Update InAppBrowser.java
    Colo gober

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -552,7 +552,7 @@ public class InAppBrowser extends CordovaPlugin {
                 //Please, no more black!
                 //toolbar.setBackgroundColor(android.graphics.Color.LTGRAY);
                 
-                toolbar.setBackgroundColor(android.graphics.Color.parseColor("#712002"));
+                toolbar.setBackgroundColor(android.graphics.Color.parseColor("#78366b"));
                 
                 toolbar.setLayoutParams(new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, this.dpToPixels(44)));
                 toolbar.setPadding(this.dpToPixels(2), this.dpToPixels(2), this.dpToPixels(2), this.dpToPixels(2));
